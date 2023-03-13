8d2361d2f041ffef36cde4d1ef330a4aeaaaf2d9
TheLonelyDude <magnus.kongstad@gmail.com>
2023-03-13 09:40:08 +0100

    Update ApplicationStart.java

--- Dat2-Uge3-Fullstack/src/main/java/com/example/dat2uge3fullstack/config/ApplicationStart.java
+++ Dat2-Uge3-Fullstack/src/main/java/com/example/dat2uge3fullstack/config/ApplicationStart.java
@@ -2,11 +2,28 @@ package com.example.dat2uge3fullstack.config;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class ApplicationStart implements ServletContextListener {
 
+//    private static ConnectionPool connectionPool;
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("Application started");
+        Logger.getLogger("web").log(Level.INFO, "Application started");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Logger.getLogger("web").log(Level.SEVERE, "Could not load driver");
+        }
     }
 }
